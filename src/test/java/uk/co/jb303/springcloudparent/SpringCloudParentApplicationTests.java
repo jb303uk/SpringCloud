13e6bc152b3b4052f6fc4a3cd46e989dc8ac5787
James Beardsley <jb303@hotmail.com>
2023-03-29 20:06:32 +0100

    First Commit Parent Project spring-cloud-config-server

---
+++ src/test/java/uk/co/jb303/springcloudparent/SpringCloudParentApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.jb303.springcloudparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
