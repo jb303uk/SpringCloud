a6a7f247c28fdd3133aca3f664817128d570ca0e
James Beardsley <jb303@hotmail.com>
2023-04-02 21:10:32 +0100

    Spring Cloud Service Registry

---
+++ spring-cloud-service-registry/src/test/java/uk/co/jb303/SpringCloudServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.jb303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
