cdf2e6b6b82bf8790001a2656b727084d0bcb886
James Beardsley <jb303@hotmail.com>
2026-08-08 13:46:35 +0100

    Working Name shown from view

--- spring-cloud-game/src/main/java/uk/gov/hmrc/controller/CatchWebController.java
+++ spring-cloud-game/src/main/java/uk/gov/hmrc/controller/CatchWebController.java
@@ -4,7 +4,7 @@ import uk.gov.hmrc.entity.Catches;
 import uk.gov.hmrc.repository.CatchView;
 import uk.gov.hmrc.repository.CatchRepository;
 
-
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -28,10 +29,17 @@ public class CatchWebController {
     }
 
     @GetMapping("/")
-    public String showInsertForm(Model model,
-    		@PageableDefault(size = 5) Pageable pageable)
-    		{
-        model.addAttribute("catches", catchRepository.findAll(Sort.by(Sort.Direction.DESC, "catchId")));
+    public String getDashboard(
+            @RequestParam(defaultValue = "0") int pageAll,
+            @RequestParam(defaultValue = "5") int sizeAll,
+            @RequestParam(defaultValue = "catchId") String sortAll,
+            @RequestParam(defaultValue = "desc") String dirAll,            
+            Model model) {
+        Sort sortOrderAll = dirAll.equalsIgnoreCase("desc") ? Sort.by(sortAll).descending() : Sort.by(sortAll).ascending();
+        Pageable pageableAll = PageRequest.of(pageAll, sizeAll, sortOrderAll);
+        model.addAttribute("allCatches", catchView.findAll(pageableAll));
+        model.addAttribute("sortAll", sortAll);
+        model.addAttribute("dirAll", dirAll);
         return "catches";
     }
 
