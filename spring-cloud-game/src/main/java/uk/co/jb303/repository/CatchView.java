d808b4f40d16a60b52c0f53c1d16c3804cb1c802
James Beardsley <jb303@hotmail.com>
2026-08-27 17:38:52 +0100

    Revert "Use Window not Page"
    This reverts commit 9675af7eaf3aa57625cf0fa153a243b93771f834.

--- spring-cloud-game/src/main/java/uk/co/jb303/repository/CatchView.java
+++ spring-cloud-game/src/main/java/uk/co/jb303/repository/CatchView.java
@@ -1,13 +1,14 @@
 package uk.co.jb303.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Window;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import uk.co.jb303.entity.CatchSummary;
 import uk.co.jb303.repository.CatchView;
 
 public interface CatchView extends PagingAndSortingRepository<CatchSummary, Integer> {
+    //Page<CatchSummary> findAll(Pageable pageable);
 
-	Window<CatchSummary> findByUserUUID(String uuid, Pageable pageable);
+	Page<CatchSummary> findByUserUUID(String uuid, Pageable pageable);
 }
